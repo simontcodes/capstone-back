d92f5c79f66cbf7442e86b22e1974c58f7450a2f
simon tang <simont.codes@gmail.com>
2022-09-26 23:36:45 -0400

    now sends email with appointment details to client

--- routes/clientsPost.js
+++ routes/clientsPost.js
@@ -9,6 +9,13 @@ const knex = require("knex")(require("../knexfile"));
 //unique ids
 const { v4: uuidv4 } = require("uuid");
 
+//sendgrid api key saved in .env file
+const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
+//importing sendgrid library
+const sgMail = require('@sendgrid/mail');
+// set api key on the api call
+sgMail.setApiKey(SENDGRID_API_KEY);
+
 router.route("/").post(async (req, res) => {
   const clientWorkExp = JSON.parse(req.body.jobs);
   const clientInfo = {
@@ -42,7 +49,9 @@ router.route("/").post(async (req, res) => {
   try {
     const resultCLient = await knex("client").insert(clientInfo);
     const createdClient = await knex("client").where({ id: resultCLient[0] });
-    const resultAppointment = await knex("appointment").insert(clientAppointment);
+    const resultAppointment = await knex("appointment").insert(
+      clientAppointment
+    );
 
     for (let i = 0; i < clientWorkExp.length; i++) {
       clientWorkExp[i].client_id = clientInfo.id;
@@ -50,7 +59,33 @@ router.route("/").post(async (req, res) => {
     for (let i = 0; i < clientWorkExp.length; i++) {
       await knex("workExp").insert(clientWorkExp[i]);
     }
-
+    // ------------------------------------------------------
+    console.log(clientInfo.email)
+    // email message format
+    const message = {
+      to: `${clientInfo.email}`,
+      from: {
+        name: "CM Immigration.Inc",
+        email: "simont.codes@gmail.com",
+      },
+          subject: "Appointment Details",
+          text: "how cool is this",
+          html: `<h1> Hi ${clientInfo.firstName} ${clientInfo.lastName}</h1>
+                  <p>We will see you on ${clientAppointment.dateOfAppointment} at ${clientAppointment.timeOfAppointment} </p>
+                  Holi estoy probando`,
+        };
+        
+        // the actual api call  that returns a promise
+        sgMail
+        .send(message)
+        .then((response) => {
+          console.log("email sent");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+        
+        // ------------------------------------------------------
     res.status(201).json(createdClient);
   } catch (error) {
     console.log(error);
