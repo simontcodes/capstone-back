86017623e6f73c1b3f5878187b4e47f81b26bfc0
simon codes <simont.codes@gmail.com>
2022-11-16 15:20:14 -0500

    change in knex file to change database

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,9 @@ module.exports = {
   client: "mysql2",
   connection: {
     host: "127.0.0.1",
-    database: "cmimmigration",
+    database: "CMIMMIGRATION",
     user: "root",
     password: "rootroot",
-    charset: "utf8"
-  }
+    charset: "utf8",
+  },
 };
